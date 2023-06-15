fc0a2a5dec95881a186193b4c13b9d7a0abbdfd7
serg-flex <doitflex.dev@gmail.com>
2023-06-15 10:28:36 +0300

    naming fixed

--- simple/README.md
+++ simple/README.md
@@ -1,5 +1,5 @@
-# Very simple example of code and decode text
+# Very simple example of encode and decode text
 
-In this example I use **_ord()_** and **_chr()_** built-in functions for code and decode text message with const **int** key.
+In this example I use **_ord()_** and **_chr()_** built-in functions for encode and decode text message with const **int** key.
 
-For coding message I just convert **ascii code** to **int** and add constant to it and then back convert values to ascii. For decoding I  subtruct constant.
+For encoding message I just convert **ascii code** to **int** and add constant to it and then back convert values to ascii. For decoding I subtruct constant.
--- simple/main.py
+++ simple/main.py
@@ -1,11 +1,11 @@
-#  Setup simple int for coding message
+#  Setup simple int for encoding message
 CODE_KEY = 5
 #  Get source message from stdin
 source = input("Enter message for coding: \n")
 
 
-#  Coding message using our CODE_KEY
-def code_source(source: str) -> str:
+#  Encoding message using our CODE_KEY
+def encode_source(source: str) -> str:
     #  Convert ascii to int and add CODE_KEY value to it
     converted = [ord(char) + CODE_KEY for char in source]
     #  Return string with new ascii codes for symbols
@@ -20,7 +20,7 @@ def decode_message(source: str) -> str:
     return "".join(chr(char) for char in converted)
 
 
-#  Print coded and decoded message
-result = code_source(source)
+#  Print encoded and decoded message
+result = encode_source(source)
 print(f"Coded message: {result}")
 print(f"Decoded message: {decode_message(result)}")
